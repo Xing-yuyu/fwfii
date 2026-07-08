dfe327ecea3ad9d3fd0da695ceb8e8c40dda9ab0
Kevin0412 <kevinzhu633@gmail.com>
2026-07-09 00:55:05 +0800

    Modernize packaging and optional imports

--- fwfii/fc/__init__.py
+++ fwfii/fc/__init__.py
@@ -5,7 +5,15 @@ from .flight import Flight
 from .advanced import *
 from .basic import * 
 from .heartbeat import *
-from .monitor import *
-from .obstacle import *
-from .detect import *
-from .odom import *
+
+
+def __getattr__(name):
+    if name == "Monitor":
+        from .monitor import Monitor
+
+        return Monitor
+    if name == "VisualOdom":
+        from .odom import VisualOdom
+
+        return VisualOdom
+    raise AttributeError("module 'fwfii.fc' has no attribute {!r}".format(name))
--- setup.py
+++ setup.py
@@ -1,36 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open("README.md", encoding="utf-8") as f:
-    long_description = f.read()
 
-setup(
-    name="fwfii",
-    version="1.0.1",
-    description="Fii Drone Flight Control SDK for F400/F600",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="fwfii",
-    url="https://github.com/yourusername/fwfii",
-    packages=find_packages(exclude=["examples", "tests"]),
-    install_requires=[
-        "pyserial>=3.5",
-    ],
-    python_requires=">=3.6",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Education",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Topic :: Education",
-        "Topic :: Scientific/Engineering",
-    ],
-    keywords="drone, flight control, education, fii, F400, F600, UAV",
-)
+setup()
